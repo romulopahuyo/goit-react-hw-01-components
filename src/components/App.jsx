3852f2fd8970fa7e615fb9f09594bce6c825982a
romulopahuyo <romulo.pahuyo@gmail.com>
2024-03-28 09:55:46 +0800

    ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+
+import MenuItem from "./MenuItem/MenuItem";
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <div>
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
+                {/* <h1> React homework template</h1> 
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi nam consequuntur voluptate obcaecati esse quaerat recusandae porro ratione at doloremque autem odit dolorum consectetur, expedita fugiat doloribus, vitae suscipit vel!</p> */}
+ </div>
   );
 };
---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const MenuItem = () => {
+    return (
+
+
+        <div style={{ marginBottom: `20px` }}>
+            <img
+                src="https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/08/Greek-Salad-6-1.jpg" alt="salad"
+                style={{height: `400px`}}
+            />
+            <h2> Green Lantern Salad</h2>
+            <p> Romaine blend, feta cheese, red onions, beets, mild peppers, black olives, grape tomatoes.
+                Served with Nikki\u2019s Greek dressing.
+            </p>
+            <span>$5</span>
+            <br>Special with Egg</br>
+            
+        </div>
+    );
+};
+
+export default MenuItem ;
